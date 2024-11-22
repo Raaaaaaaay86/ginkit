c64dfa84796e988d7b39a7c59df280423ca5b752
Ray Lin <raylincontact@icloud.com>
2024-11-22 09:42:23 +0800

    refactor: remove underscore in package name

--- example/route/main.go
+++ example/route/main.go
@@ -7,8 +7,8 @@ import (
 	"github.com/gin-gonic/gin"
 	"github.com/raaaaaaaay86/ginutil"
 	"github.com/raaaaaaaay86/ginutil/example/route/controller"
-	"github.com/raaaaaaaay86/ginutil/example/route/persistence/persistence_impl"
-	"github.com/raaaaaaaay86/ginutil/example/route/service/service_impl"
+	"github.com/raaaaaaaay86/ginutil/example/route/persistence/persistenceimpl"
+	"github.com/raaaaaaaay86/ginutil/example/route/service/serviceimpl"
 )
 
 func main() {
@@ -22,7 +22,7 @@ func Run(ctx context.Context) error {
 
 	ginutil.RouteGroups{
 		controller.NewUser(),
-		controller.NewStore(service_impl.NewStore(persistence_impl.NewStore())),
+		controller.NewStore(serviceimpl.NewStore(persistenceimpl.NewStore())),
 	}.Register(engine)
 
 	return engine.Run(":8080")
--- example/route/persistence/persistenceimpl/store.go
+++ example/route/persistence/persistenceimpl/store.go
@@ -1,4 +1,4 @@
-package persistence_impl
+package persistenceimpl
 
 import (
 	"fmt"
